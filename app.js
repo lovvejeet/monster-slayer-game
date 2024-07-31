dadbe8d63bccff41e0aee3793e960d0523a1990a
lovvejeet <lovejeetsandhu8194@gmail.com>
2024-07-31 15:06:26 +0530

    added healPlayer, surrender and new start game functionality

--- app.js
+++ app.js
@@ -8,17 +8,46 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       currentRound: 0,
+      winner: null,
     };
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.monsterHealth + "%" };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
       return { width: this.playerHealth + "%" };
     },
     mayUseSpecialAttack() {
       return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      // here "value" is nothing but playerHealth from data()
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // it's draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      // here "value" is nothing but monsterHealth from data()
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // it's draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // monster lost
+        this.winner = "player";
+      }
     },
   },
   methods: {
@@ -37,6 +66,25 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
       this.attackPlayer();
+    },
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.attackPlayer();
+    },
+    surrender() {
+      this.winner = "monster";
+    },
+    startGame() {
+      (this.playerHealth = 100),
+        (this.monsterHealth = 100),
+        (this.winner = null),
+        (this.currentRound = 0);
     },
   },
 });

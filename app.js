b2a55c322ee08fc69fc2efb54f569676d5e1ac49
lovvejeet <lovejeetsandhu8194@gmail.com>
2024-07-31 12:09:45 +0530

    added dynamic health bar and special attack

--- app.js
+++ app.js
@@ -7,19 +7,36 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
-      this.attackPlayer(
-        
-      )
+      this.attackPlayer();
     },
     attackPlayer() {
-      const attackValue = getRandomValue(8,15);
+      const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
     },
   },
 });
